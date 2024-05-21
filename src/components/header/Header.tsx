e5e7d4ec5acd50fb87f37ea486c6edba2a1c54ec
Ivanilso da Silva Conceição Filho <ivanilso.silva@g8solucoes.com.br>
2024-05-21 16:40:28 -0300

    REFAC: Correção da imagem

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,7 @@
-import Logo from "./../../assets/img/logo-compacto.png";
-
 const Header = () => {
     return (
         <header className="flex h-32 bg-blue-secondary theme-padding">
-            <img className="h-24 m-auto" src={Logo} alt="Logo da empresa" />
+            <img className="h-24 m-auto" src="https://treinamentos.procertsst.online/pluginfile.php/1/core_admin/logocompact/300x300/1715132605/logo%20-%20compacto.png" alt="Logo da empresa" />
         </header>
     )
 }
